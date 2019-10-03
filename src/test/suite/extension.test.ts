f00e72d71ac021496bc883cbf6db58f7f265875d
CatStarwind <catstarwind@gmail.com>
2019-10-03 14:02:10 -0700

    Finished tests for enabled and single dest sync.

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -2,16 +2,18 @@ import * as assert from 'assert';
 import { before, after } from 'mocha';
 import * as vscode from 'vscode';
 import { FileSync, Mapping } from '../../fileSync';
-import { TextEncoder } from 'util';
 
 suite('Extension Test Suite', () => {
 	let fs: FileSync;
-	let fsconfig: vscode.WorkspaceConfiguration;
+	let fsconfig = vscode.workspace.getConfiguration("filesync");
 	let origMaps: Mapping[];
 	let rootPath: string = __dirname + "\\TestBed"; // __dirname = "c:\Projects\FileSync\out\test\suite\TestBed"
-	let devOrange: vscode.Uri = vscode.Uri.file(rootPath+"\\Dev\\Orange");
-	let stageOrange:  vscode.Uri = vscode.Uri.file(rootPath+"\\Stage\\Orange");
-	let scream = function(ah: any){ console.log("scream: " + (ah ? ah : "√")); };
+	let testbed = {
+		"folder": function(loc: string): vscode.Uri { return vscode.Uri.file(rootPath+"\\"+loc+"\\Orange"); }
+		,"file": function(loc: string): vscode.Uri { return vscode.Uri.file(this.folder(loc).fsPath + "\\test.htm"); }
+	};
+	let scream = function(ah: any){ if(false) { console.log("scream: " + (ah ? ah : "√")); } };
+	let sleep = function(ms: number){ return new Promise(r => setTimeout(r,ms)); };
 
 
 	before(async () => {
@@ -24,18 +26,13 @@ suite('Extension Test Suite', () => {
 		}
 
 		//Build TestBed
-		await vscode.workspace.fs.createDirectory(devOrange)
-			.then(scream, scream);
-		let edit = new vscode.WorkspaceEdit();
-		edit.createFile(vscode.Uri.file(devOrange+"\\test.htm"), {ignoreIfExists: true, overwrite:true});
-		await vscode.workspace.applyEdit(edit).then(scream,scream);
-		await vscode.workspace.fs.writeFile(vscode.Uri.file(devOrange+"\\test.htm"), (new TextEncoder()).encode("<html></html>"))
-			.then(scream, scream);
-		await vscode.workspace.fs.delete(stageOrange, {recursive: true})
-			.then(scream, scream);
+		await vscode.workspace.fs.createDirectory(testbed.folder("dev")).then(scream, scream);
+		await vscode.workspace.fs.delete(testbed.folder("stage"), {recursive: true}).then(scream, scream);
+		let wse = new vscode.WorkspaceEdit();
+		wse.createFile(testbed.file("dev"), {overwrite: true });
+		await vscode.workspace.applyEdit(wse).then(scream,scream);
 
 		//Get current mappings and save them.
-		fsconfig = vscode.workspace.getConfiguration("filesync");
 		origMaps = fsconfig.get<Mapping[]>("mappings") || <Mapping[]>[];
 
 		//Update mappings for tests.
@@ -48,26 +45,33 @@ suite('Extension Test Suite', () => {
 
 	after(async () => {
 		//Bring back original mappings
-		await fsconfig.update("mappings", origMaps, true)
-			.then(scream,scream);
+		await fsconfig.update("mappings", origMaps, true).then(scream,scream);
 	});
 
 	test('Mapped Folder Enabled', () => {
+		assert.strictEqual(vscode.workspace.name, "Orange");
 		assert.ok(fs.enabled);
 	});
 
-	test('Mapped Folder Sync', async () => {
-		let testDoc: vscode.TextDocument = await vscode.workspace.openTextDocument("test.htm").then(scream,scream).then();
-		let editor = vscode.window.activeTextEditor;
-		if(editor){
-			await editor.edit(eb => {
-				eb.insert((new vscode.Position(0,5)),"<p>hi</p>");
+	test('Mapped Folder Sync', async function() {
+		this.timeout(5000);
+
+		//Dirty the file
+		await vscode.window.showTextDocument(testbed.file("dev")).then(async editor => {
+			await editor.edit(builder => {
+				builder.insert((new vscode.Position(0,0)), "<html></html>");
 			}).then(scream,scream);
-		} else {
-			assert.fail("No Editor");
-		}
+		},scream);
 
-		await testDoc.save().then(scream, scream);
+		//Save to trigger the Sync
+		await vscode.workspace.saveAll().then(scream,scream);
+		await sleep(1000);
+
+		//Check destination
+		await vscode.workspace.fs.stat(testbed.file("stage")).then(file => {
+			assert.ok(file.size > 0);
+		}, err => {
+			assert.fail(err.message);
+		});
 	});
-
 });
